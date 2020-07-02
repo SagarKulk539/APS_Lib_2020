6c7a731ade1dad78078edb8c07f87b45435a6461
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2020-07-02 09:03:05 +0530

    First commit

---
+++ Codeforces/MagicalSticks_654_A.java
@@ -0,0 +1,25 @@
+/*
+APS-2020
+Problem Description, Input, Output : https://codeforces.com/contest/1371/problem/A
+Code by : Sagar Kulkarni
+*/
+
+import java.util.*;
+
+public class MagicalSticks_654_A
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        for(int iter1=0;iter1<T;iter1++)
+        {
+            long n=sc.nextLong();
+            if((n&1)==1)
+                System.out.println((n+1)/2);
+            else
+                System.out.println(n/2);
+        }
+        sc.close();
+    }
+}
