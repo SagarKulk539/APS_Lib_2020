3e7e736df0ff6e66ba3b9d0039c975b026102913
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2020-07-12 01:29:38 +0530

    First Commit

---
+++ Codeforces/omkarArray_655_A.cpp
@@ -0,0 +1,30 @@
+/*
+APS-2020
+Problem Description, Input, Output : https://codeforces.com/contest/1372/problem/A
+Code by : Sagar Kulkarni
+*/
+
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int t;
+    cin>>t;
+
+    for(int i=0;i<t;i++)
+    {
+        int n;
+        cin>>n;
+
+        for(int j=0;j<n;j++)
+            cout<<1<<" ";
+        cout<<"\n";
+    }
+
+    return 0;
+}
