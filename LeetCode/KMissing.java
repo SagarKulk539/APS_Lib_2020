d02c6e9288c08b1941be78d917d3a8fd65b61e9b
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2020-08-08 20:29:21 +0530

    First Commit

---
+++ LeetCode/KMissing.java
@@ -0,0 +1,30 @@
+/*
+APS-2020
+Problem Description, Input, Output : https://leetcode.com/contest/biweekly-contest-32/problems/kth-missing-positive-number/
+Code by : Sagar Kulkarni
+*/
+
+class KMissing 
+{
+    public int findKthPositive(int[] arr, int k) 
+    {
+        int[] tempArr=new int[10001];
+        for(int i=0;i<arr.length;i++)
+            tempArr[arr[i]]=1;
+        
+        int count=0,res=0;
+        for(int i=1;i<=10000;i++)
+        {
+            if(tempArr[i]==0)
+            {
+                count++;
+                if(count==k)
+                {
+                    res=i;
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+}
