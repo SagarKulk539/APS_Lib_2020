15e2f629ebc86d900cb55daa4baf0f6acc979cec
Sagar Kulkarni <44294928+SagarKulk539@users.noreply.github.com>
2020-08-08 20:30:49 +0530

    First Commit

--- LeetCode/KMissing.java
+++ LeetCode/KMissing.java
@@ -8,12 +8,12 @@ class KMissing
 {
     public int findKthPositive(int[] arr, int k) 
     {
-        int[] tempArr=new int[10001];
+        int[] tempArr=new int[2001];
         for(int i=0;i<arr.length;i++)
             tempArr[arr[i]]=1;
         
         int count=0,res=0;
-        for(int i=1;i<=10000;i++)
+        for(int i=1;i<=2000;i++)
         {
             if(tempArr[i]==0)
             {
